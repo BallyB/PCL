36e4afb8ea6e3afdf26b0d7208f79f858982b612
adrien <adrien@arkanoid.loria.fr>
2017-06-09 17:46:52 +0200

    ICP ptp done

--- main.cpp
+++ main.cpp
@@ -1,30 +1,47 @@
 #include <iostream>
 #include <pcl/io/pcd_io.h>
 #include <pcl/point_types.h>
+#include <pcl/registration/icp.h>
 
 int
-  main (int argc, char** argv)
+ main (int argc, char** argv)
 {
-  pcl::PointCloud<pcl::PointXYZ> cloud;
+  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_in (new pcl::PointCloud<pcl::PointXYZ>);
+  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_out (new pcl::PointCloud<pcl::PointXYZ>);
 
-  // Fill in the cloud data
-  cloud.width    = 5;
-  cloud.height   = 1;
-  cloud.is_dense = false;
-  cloud.points.resize (cloud.width * cloud.height);
-
-  for (size_t i = 0; i < cloud.points.size (); ++i)
+  // Fill in the CloudIn data
+  cloud_in->width    = 5;
+  cloud_in->height   = 1;
+  cloud_in->is_dense = false;
+  cloud_in->points.resize (cloud_in->width * cloud_in->height);
+  for (size_t i = 0; i < cloud_in->points.size (); ++i)
   {
-    cloud.points[i].x = 1024 * rand () / (RAND_MAX + 1.0f);
-    cloud.points[i].y = 1024 * rand () / (RAND_MAX + 1.0f);
-    cloud.points[i].z = 1024 * rand () / (RAND_MAX + 1.0f);
+    cloud_in->points[i].x = 1024 * rand () / (RAND_MAX + 1.0f);
+    cloud_in->points[i].y = 1024 * rand () / (RAND_MAX + 1.0f);
+    cloud_in->points[i].z = 1024 * rand () / (RAND_MAX + 1.0f);
   }
+  std::cout << "Saved " << cloud_in->points.size () << " data points to input:"
+      << std::endl;
+  for (size_t i = 0; i < cloud_in->points.size (); ++i) std::cout << "    " <<
+      cloud_in->points[i].x << " " << cloud_in->points[i].y << " " <<
+      cloud_in->points[i].z << std::endl;
+  *cloud_out = *cloud_in;
+  std::cout << "size:" << cloud_out->points.size() << std::endl;
+  for (size_t i = 0; i < cloud_in->points.size (); ++i)
+    cloud_out->points[i].x = cloud_in->points[i].x + 0.7f;
+  std::cout << "Transformed " << cloud_in->points.size () << " data points:"
+      << std::endl;
+  for (size_t i = 0; i < cloud_out->points.size (); ++i)
+    std::cout << "    " << cloud_out->points[i].x << " " <<
+      cloud_out->points[i].y << " " << cloud_out->points[i].z << std::endl;
+  pcl::IterativeClosestPoint<pcl::PointXYZ, pcl::PointXYZ> icp;
+  icp.setInputCloud(cloud_in);
+  icp.setInputTarget(cloud_out);
+  pcl::PointCloud<pcl::PointXYZ> Final;
+  icp.align(Final);
+  std::cout << "has converged:" << icp.hasConverged() << " score: " <<
+  icp.getFitnessScore() << std::endl;
+  std::cout << icp.getFinalTransformation() << std::endl;
 
-  pcl::io::savePCDFileASCII ("test_pcd.pcd", cloud);
-  std::cerr << "Saved " << cloud.points.size () << " data points to test_pcd.pcd." << std::endl;
-
-  for (size_t i = 0; i < cloud.points.size (); ++i)
-    std::cerr << "    " << cloud.points[i].x << " " << cloud.points[i].y << " " << cloud.points[i].z << std::endl;
-
-  return (0);
+ return (0);
 }
